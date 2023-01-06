48c4f3fb99e1612a237e7f307cd3ce2dc95ebd2f
Aladaner <solovej555@gmail.com>
2023-01-06 23:03:12 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,13 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
+	publicPath:
+		process.env.NODE_ENV === 'production'
+			? '/vue-youtube_thumbnail_download'
+			: '/',
 	transpileDependencies: true,
 	css: {
 		loaderOptions: {
-			scss: {
-				
-			},
+			scss: {},
 		},
 	},
 });
